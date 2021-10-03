b60c0ecbb25d224585a0273bbce0a18d1333f247
devalalbin <vala@student.chalmers.se>
2021-10-03 16:50:23 +0200

    bug fix

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -9,7 +9,7 @@ namespace Hangman
         static void Main(string[] args)
         {
             int userChoice;
-            
+
             while (true)
             {
 
@@ -27,8 +27,8 @@ namespace Hangman
                         case 1:
                             HangmanGame();
                             break;
-                        
-                        
+
+
                         default:
                             Console.WriteLine("Please enter a valid number! (0 to exit)");
                             break;
@@ -98,6 +98,7 @@ namespace Hangman
 
                     Console.WriteLine("hey good job you guessed every letter right! " + correctString); //user won by guessing every letter correct
                     CheckIfGameOver(maxGuesses, nrOfGuesses, true, answer);
+                    break;
 
                 }
                 userGuess = GetUserGuess(correctLetters, incorrectLetters); // get user guess here
@@ -129,6 +130,7 @@ namespace Hangman
                     }
                 }
 
+
                 end = CheckIfGameOver(maxGuesses, nrOfGuesses, win, answer);
                 if (end == true)
                 {
@@ -150,8 +152,8 @@ namespace Hangman
                 Random rnd = new Random();
                 string[] randomAnswer =
                  {"CAT","DOG","SHARK","BIRD","BEAR","FISH","CHICKEN","HORSE","LOBSTER","MONKEY"}; // array of 10 strings
-              
-               return randomAnswer[rnd.Next(0,9)];  // returns a random string from the array 10 choices
+
+                return randomAnswer[rnd.Next(0, 9)];  // returns a random string from the array 10 choices
             }
         }
 
@@ -159,41 +161,42 @@ namespace Hangman
         {
             // first check if user has already guessed that letter, if so make user make a new guess
 
-            
+
             string userGuess = Console.ReadLine().ToUpper();
             if (userGuess == "0") Environment.Exit(0);
-            if (userGuess.Length == 1) { 
-            while (incorrectLetters.ToString().Contains(userGuess)) // while they are trying to guess a letter that they already guessed make them guess again
+            if (userGuess.Length == 1)
             {
-                Console.WriteLine("You already guessed that letter, Please make a new guess :");
+                while (incorrectLetters.ToString().Contains(userGuess)) // while they are trying to guess a letter that they already guessed make them guess again
+                {
+                    Console.WriteLine("You already guessed that letter, Please make a new guess :");
                     Console.WriteLine("Incorrect letters guessed :" + incorrectLetters);
                     userGuess = Console.ReadLine().ToUpper();
-                    
+
 
                 }
-            for (int i = 0; i < correctLetters.Length; i++) //iterate through the correct guess array to make sure user dosent guess same letter twice
-            {
+                for (int i = 0; i < correctLetters.Length; i++) //iterate through the correct guess array to make sure user dosent guess same letter twice
+                {
                     while (userGuess[0] == correctLetters[i])
                     {
                         Console.WriteLine("That letter is already in the word! Please make a new guess :");
                         Console.WriteLine(correctLetters);
                         userGuess = Console.ReadLine().ToUpper();
-                        
+
                     }
                 }
-            }                               
+            }
             return userGuess;
         }
-        
-        static char[] GuessTrueOrFalse(string userGuess,StringBuilder incorrectletter, string answer, char[] correctLetters)
+
+        static char[] GuessTrueOrFalse(string userGuess, StringBuilder incorrectletter, string answer, char[] correctLetters)
         {
-            
+
             bool correctGuess = false;
             for (int i = 0; i < answer.Length; i++)  // iterate through the array to check to see if the user answer is correct
             {
-                
+
                 //Console.WriteLine("this is the comparison" + userGuess + answer[i]);
-                if(string.Equals(userGuess[0], answer[i])) // Check if guess and answer is equal, if so add it to the CHAR array
+                if (string.Equals(userGuess[0], answer[i])) // Check if guess and answer is equal, if so add it to the CHAR array
                 {
                     Console.WriteLine(userGuess + " IS A CORRECT GUESS");
                     Console.WriteLine("Incorrect letters guessed :" + incorrectletter);
@@ -201,18 +204,19 @@ namespace Hangman
                     Console.WriteLine(correctLetters);
                     correctGuess = true;
                 }
-               
+
             }
-             if(correctGuess == false)
+            if (correctGuess == false)
             {
                 incorrectletter.Append(userGuess[0]);
                 Console.WriteLine("Your guess was incorrect");
-                Console.WriteLine("Incorrect letters guessed :"+incorrectletter);
+                Console.WriteLine("Incorrect letters guessed :" + incorrectletter);
 
             }
             return correctLetters;
         }
-        static bool CheckIfGameOver(int maxguesses, int guesses, bool win, string answer) { //returns win or loss bool
+        static bool CheckIfGameOver(int maxguesses, int guesses, bool win, string answer)
+        { //returns win or loss bool
 
             if (win == false)
             {
@@ -238,7 +242,7 @@ namespace Hangman
 
             return false; // end = false if endcondition is not met
         }
-        
+
     }
 
 }
